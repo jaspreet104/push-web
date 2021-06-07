f516b6fb832c47b6b4232bef84797dd0c7fd5c34
jaspreet a singh <jaspreet_a_singh@ultimatesoftware.com>
2021-06-07 14:58:54 +0530

    fix

--- mysw.js
+++ mysw.js
@@ -1,3 +1,4 @@
 self.addEventListener('push', () => {
-    self.registration.sendNotification('test msg from sw js', {})
+    //self.registration.sendNotification('test msg from sw js', {})
+    self.registration.showNotification('hello world')
 });
--- push.js
+++ push.js
@@ -8,6 +8,17 @@ let vapIdKeys = {
 
 push.setVapidDetails('mailto:test@test.com', vapIdKeys.publicKey, vapIdKeys.privateKey);
 
-let sub = {};
+let sub = {
+    "endpoint":
+        "https://fcm.googleapis.com/fcm/send/f-DF6iiixsU:APA91bGVmHX8ENfVk_yGiR7wtAPGBA39mpcYrubqco77VurH-jJud4Vq1ScmCOIFBlRcFO4TZBPDKDvhWbA-iGJO6fJX2GMfAMWRnR5a0BWyNJy8H4DKeCZLVew5ByVkCSdT8Fd7-hvx",
+    "expirationTime":
+        null,
+    "keys":{
+        "p256dh":
+            "BJTmM9DKCR-rk8jOUIbboXz2EYKAzxLVKDTpKEV8qn6ET8VLeA8THOZ_pqCDrgXAay6kMY6dRO7ny6QYCZI20Ho",
+        "auth":
+            "9jR7RnmNfEW9V5tSm_JANw"
+    }
+};
 
 push.sendNotification(sub, 'test msg from push js')
